8e1d2ee4ae3bcda9ee24376caaaecd443b95c0a9
alanev <e@alanev.ru>
2015-08-04 10:08:55 +0300

    White and black filter
    Added shortcut:
    filter: white() -> filter: brightness(0) invert(100%)
    filter: black() -> filter: brightness(0)

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('PLUGIN_NAME', function (opts) {
+module.exports = postcss.plugin('postcss-filter', function (opts) {
     opts = opts || {};
 
-    // Work with options here
-
     return function (css) {
+        css.eachDecl('filter', function (decl) {
+            var value = decl.value;
 
-        // Transform CSS AST here
-
+            if (value) {
+                if (value.indexOf('black') !== -1) {
+                    decl.value = 'brightness(0)';
+                }
+                if (value.indexOf('white') !== -1) {
+                    decl.value = 'brightness(0) invert(100%)';
+                }
+            }
+        });
     };
 });
